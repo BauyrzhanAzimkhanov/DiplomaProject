2427c74ebc6d76e44a8e65dcacf6bcead2860bba
Азимханов Бауыржан <bauyrzhanaz2@halykbank.kz>
2023-04-21 17:19:13 +0600

    Removed/commented unused code.

--- api/urls.py
+++ api/urls.py
@@ -14,14 +14,14 @@ urlpatterns = [
     # path('products/', product_list),
     # path('products/<int:pk>/', product_detail),
 
-    path('shipping/', shipping_list),
-    path('shipping/<int:pk>/', shipping_detail),
+    # path('shipping/', shipping_list),
+    # path('shipping/<int:pk>/', shipping_detail),
 
-    path('images/', image_list),
-    path('images/<int:pk>/', image_detail),
+    # path('images/', image_list),
+    # path('images/<int:pk>/', image_detail),
 
-    path('comments/', comment_list),
-    path('comments/<int:pk>/', comment_detail),
+    # path('comments/', comment_list),
+    # path('comments/<int:pk>/', comment_detail),
 
     # CBV
     path('categories/', CategoryListAPIView.as_view()),
@@ -31,22 +31,22 @@ urlpatterns = [
     path('products/<int:pk>/', ProductDetailAPIView.as_view()),
 
     path('users/', UserListAPIView.as_view()),
-    path('users/', UserDetailAPIView.as_view()),
+    path('users/<int:pk>/', UserDetailAPIView.as_view()),
 
     path('courses/', CourseListAPIView.as_view()),
-    path('courses/', CourseDetailAPIView.as_view()),
+    path('courses/<int:pk>/', CourseDetailAPIView.as_view()),
 
     path('lectures/', LectureListAPIView.as_view()),
-    path('lectures/', LectureDetailAPIView.as_view()),
+    path('lectures/<int:pk>/', LectureDetailAPIView.as_view()),
 
     path('absences/', AbsencesListAPIView.as_view()),
-    path('absences/', AbsencesDetailAPIView.as_view()),
+    path('absences/<int:pk>/', AbsencesDetailAPIView.as_view()),
 
     path('lates/', LatesListAPIView.as_view()),
-    path('lates/', LatesDetailAPIView.as_view()),
+    path('lates/<int:pk>/', LatesDetailAPIView.as_view()),
 
     path('marks/', MarksListAPIView.as_view()),
-    path('marks/', MarksDetailAPIView.as_view()),
+    path('marks/<int:pk>/', MarksDetailAPIView.as_view()),
 
     path('home_works/', HomeWorksListAPIView.as_view()),
     path('home_works/', HomeWorksDetailAPIView.as_view()),
