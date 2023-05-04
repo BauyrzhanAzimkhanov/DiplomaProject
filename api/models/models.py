d482220b4af87ecbe536db9f23f8bd7427b68605
Азимханов Бауыржан <bauyrzhanaz2@halykbank.kz>
2023-05-04 15:01:37 +0600

    Models finished and db_configuration was updated.

--- api/models/models.py
+++ api/models/models.py
@@ -2,39 +2,6 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-class Discipline(models.Model):
-    name = models.CharField(max_length=128)
-    code = models.CharField(max_length=8)
-    description = models.TextField(default='')
-    
-    class Meta:
-        verbose_name = 'Discipline'
-        verbose_name_plural = 'Disciplines'
-    
-    def __str__(self):
-        return f'{self.id}: {self.code} | {self.name} - {self.description}.'
-
-class User(models.Model):
-    kbtuId = models.CharField(max_length=10)
-    firstName = models.CharField(max_length=30)
-    middleName = models.CharField(max_length=30)
-    lastName = models.CharField(max_length=30)
-    yearOfStudy = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(7)])
-    isTeacher = models.BooleanField(default=False)
-    isAssistant = models.BooleanField(default=False)
-    isAdmin = models.BooleanField(default=False)
-    gpa = models.FloatField(default=1.0, validators=[MinValueValidator(1.0), MaxValueValidator(4.0)])
-    absenceCount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(72)])
-    finishedCourses = models.ManyToManyField(Discipline, on_delete=models.CASCADE, null=True, related_name='finishedCources')
-    studyingCourseCode = models.ForeignKey(Discipline, on_delete=models.CASCADE, null=True, related_name='studyingCourseCode')
-
-    class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
-
-    def __str__(self):
-        return f'{self.id}: {self.firstName} {self.lastName} : id={self.kbtuId}'
-
 class Semester(models.Model):
     name = models.CharField(max_length=20)
     startingDate = models.DateField()
@@ -46,11 +13,11 @@ class Semester(models.Model):
     
     def __str__(self):
         return f'{self.id}: {self.name} starting at {self.startingDate.__str__} untill {self.endingDate.__str__}'
-
+    
 class Course(models.Model):
-    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, null=True, related_name='semester')
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, null=True, related_name='courses')
     courseCode = models.CharField(max_length=8)
-    descritpion = models.TextField(default='')
+    description = models.TextField(default='')
     prerequisites = models.ForeignKey('self', on_delete=models.CASCADE, null=True, related_name='prerequisites')
 
     class Meta:
@@ -60,6 +27,26 @@ class Course(models.Model):
     def __str__(self):
         return f'{self.id}: {self.courseCode}'
 
+class User(models.Model):
+    kbtuId = models.CharField(max_length=10)
+    firstName = models.CharField(max_length=30)
+    middleName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    yearOfStudy = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(7)])
+    isTeacher = models.BooleanField(default=False)
+    isAssistant = models.BooleanField(default=False)
+    isAdmin = models.BooleanField(default=False)
+    gpa = models.FloatField(default=1.0, validators=[MinValueValidator(1.0), MaxValueValidator(4.0)])
+    finishedCourses = models.ManyToManyField(Course, on_delete=models.CASCADE, null=True, related_name='courseFinishedUsers')
+    studyingCourse = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, related_name='courseStudyingUser')
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return f'{self.id}: {self.firstName} {self.lastName} : id={self.kbtuId}'
+
 class Lecture(models.Model):
     theme = models.TextField('')
     topics = models.TextField('')
@@ -68,7 +55,7 @@ class Lecture(models.Model):
     startingTime = models.TimeField()
     endingTime = models.TimeField()
     classRoom = models.CharField(max_length=32)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, related_name='course')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, related_name='lectures')
     isLecture = models.BooleanField(default=False)
     isPractice = models.BooleanField(default=False)
     isMidterm = models.BooleanField(default=False)
@@ -84,13 +71,59 @@ class Lecture(models.Model):
         return f'{self.id}: {self.studyWeek} | {self.course}'
 
 class AbsenceOrLate(models.Model):
-    students = models.ManyToManyField(User, on_delete=models.CASCADE, null=True, related_name='students')
-    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE, null=True, related_name='lectures')
-
+    student = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='absenceOrLate')
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE, null=True, related_name='absentOrLateStudents')
+    isAbsence = models.BooleanField(default=False)
 
     class Meta:
-        verbose_name = 'Absence'
-        verbose_name_plural = 'Absence'
+        verbose_name = 'Absence or late'
+        verbose_name_plural = 'Absences or lates'
 
     def __str__(self):
         return f'{self.id}: '
+
+class Work(models.Model):
+    topic = models.TextField('')
+    description = models.TextField('')
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE, null=True, related_name='works')
+    startDate = models.DateField()
+    deadlineDate = models.DateField()
+    startingTime = models.TimeField()
+    endingTime = models.TimeField()
+    isHomeWork = models.BooleanField(default=False)
+    isLecture = models.BooleanField(default=False)
+    isPractice = models.BooleanField(default=False)
+    isMidterm = models.BooleanField(default=False)
+    isFinal = models.BooleanField(default=False)
+    isFX = models.BooleanField(default=False)
+    isQuiz = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Work'
+        verbose_name_plural = 'Works'
+
+    def __str__(self):
+        return f'{self.id}: {self.topic}'
+
+class WorkMaterial(models.Model):
+    work = models.ForeignKey(Work, on_delete=models.CASCADE, null=True, related_name='workMaterial')
+    binData = models.BinaryField()
+
+    class Meta:
+        verbose_name = 'Work material'
+        verbose_name_plural = 'Work materials'
+
+    def __str__(self):
+        return f'{self.id}: {self.work}'
+
+class Mark(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='marks')
+    work = models.ForeignKey(Work, on_delete=models.CASCADE, null=True, related_name='marks')
+    workMark = models.FloatField(default=0.0, validators=[MinValueValidator(0.0), MaxValueValidator(100.0)])
+
+    class Meta:
+        verbose_name = 'Mark'
+        verbose_name_plural = 'Marks'
+
+    def __str__(self):
+        return f'{self.id}: {self.work} - {self.workMark}'
